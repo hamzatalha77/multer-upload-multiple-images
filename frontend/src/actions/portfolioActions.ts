ab5ebf24e3efdeeec7040aeb21927dce01c51356
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-27 20:35:39 +0100

    commit

--- frontend/src/actions/portfolioActions.ts
+++ frontend/src/actions/portfolioActions.ts
@@ -1,44 +1,9 @@
-// import { Dispatch } from 'redux'
-// import axios from 'axios'
-// import {
-//   PORTFOLIOS_CREATE_REQUEST,
-//   PORTFOLIOS_CREATE_SUCCESS,
-//   PORTFOLIOS_CREATE_FAIL,
-// } from '../constants/portfolioConstants'
-
-// export const createPortfolio =
-//   (portfolioData: any) =>
-//   async (dispatch: Dispatch): Promise<any> => {
-//     try {
-//       dispatch({ type: PORTFOLIOS_CREATE_REQUEST })
-
-//       // Make API call using axios
-//       const { data } = await axios.post('/api/portfolios', portfolioData)
-
-//       dispatch({
-//         type: PORTFOLIOS_CREATE_SUCCESS,
-//         payload: data,
-//       })
-
-//       return { success: true } // Return an action object
-//     } catch (error: any) {
-//       dispatch({
-//         type: PORTFOLIOS_CREATE_FAIL,
-//         payload:
-//           error.response && error.response.data.message
-//             ? error.response.data.message
-//             : error.message,
-//       })
-
-//       return { success: false } // Return an action object
-//     }
-//   }
 import axios from 'axios'
 import { Dispatch } from 'redux'
 import {
   PORTFOLIOS_CREATE_REQUEST,
   PORTFOLIOS_CREATE_SUCCESS,
-  PORTFOLIOS_CREATE_FAIL,
+  PORTFOLIOS_CREATE_FAIL
 } from '../constants/portfolioConstants'
 
 export const createPortfolio =
@@ -56,7 +21,7 @@ export const createPortfolio =
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
-            : error.message,
+            : error.message
       })
     }
   }
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { portfolioCreateReducer } from './reducers/portfolioReducers'
 const reducer = combineReducers({
-  portfolioCreate: portfolioCreateReducer,
+  portfolioCreate: portfolioCreateReducer
 })
 
 const initialState = {}
@@ -16,28 +16,3 @@ const store = createStore(
 )
 
 export default store
-// import {
-//   createStore,
-//   combineReducers,
-//   applyMiddleware,
-//   Middleware,
-// } from 'redux'
-// import thunk, { ThunkMiddleware } from 'redux-thunk'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-
-// // define the root state type
-// type RootState = {}
-
-// const reducer = combineReducers<RootState>({})
-
-// const initialState: RootState = {}
-// const middleware: Array<Middleware<{}, RootState>> = [
-//   thunk as ThunkMiddleware<RootState, any>,
-// ]
-
-// const store = createStore(
-//   reducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// )
-// export default store
